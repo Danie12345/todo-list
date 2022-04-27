1aa1d253f53c8677549f651f3ea5ddb23aadb453
Daniel Malo <danielmaloosorio@gmail.com>
2022-04-27 18:31:06 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
