2442989fcfdbce7f9ab72c31b17ba509850e3ef9
Daniel Malo <danielmaloosorio@gmail.com>
2022-04-27 18:38:11 -0500

    Change devserver to src

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './src',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
